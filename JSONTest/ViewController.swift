aecb9f135caa7958338de2c8ca9555ae6147cbda
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-04-01 21:49:43 +0430

    :)

--- JSONTest/ViewController.swift
+++ JSONTest/ViewController.swift
@@ -13,8 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        
+       let url = URL(string: "http://192.168.64.3")!
+           
+               let task = URLSession.shared.dataTask(with: url) { (data, response, erorr) in
+                  
+                            do {
+                                let jsonDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    
+                    let studentArray = jsonDictionary["student"] as? [[String:Any]]
+                                
+                                for item in studentArray! {
+                                    
+                                    print("Name = \(item["fname"] as! String ) lastName = \(item["lname"] as! String ) age  = \(item["age"]! )")
+                    }
+                                
+                    
+                                
+                    
+                    
+                }catch {
+                    print("Error")
+                }
 
+                
+               }
 
+                task.resume()
 }
 
+}
